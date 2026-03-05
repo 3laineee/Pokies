22757410597a96354351f60906016e43c739606f
3laineee <chenel@wgc.school.nz>
2026-03-05 14:36:38 +1300

    added 5 rows

--- Plinko 3.0.py
+++ Plinko 3.0.py
@@ -21,7 +21,7 @@ bet = 0
 #setting up fucntions
 
 #winning functim
-def win_1():
+def win():
     bet = bet*10
     balance += bet
     print("Ding Ding Ding!!!")
@@ -37,25 +37,8 @@ def lose():
 
 #shuffling the small lists inside the big list slot machine using random
 def shuffle():
-    random.shuffle(slot_machine[0])
-    random.shuffle(slot_machine[1])
-    random.shuffle(slot_machine[2])
-    random.shuffle(slot_machine[3])
-    random.shuffle(slot_machine[4])
-    random.shuffle(slot_machine[5])
-    random.shuffle(slot_machine[6])
-    random.shuffle(slot_machine[7])
-    random.shuffle(slot_machine[8])
-    random.shuffle(slot_machine[9])
-    random.shuffle(slot_machine[10])
-    random.shuffle(slot_machine[11])
-    random.shuffle(slot_machine[12])
-    random.shuffle(slot_machine[13])
-    random.shuffle(slot_machine[14])
-
-
-
-
+    for i in slot_machine:
+        random.shuffle(i)
 
 #shows you the symbols:
 def plinko():
@@ -67,7 +50,7 @@ while again == 'T' and balance > 0:
     shuffle()
     print("Your current balance is ${}.".format(balance))
     bet=int(input(("How much money are you betting?:$ ")))
-    rows=int(input("How much rows are you betting? (1-3): "))
+    rows=int(input("How much rows are you betting? (1-5): "))
 
 #checks if their bet is bigger than their balance or not
     if rows*bet > balance:
@@ -81,29 +64,56 @@ while again == 'T' and balance > 0:
         #checks how much rows their betting
         if rows == 1:
         #checks the slots for winning or losing
-            if slot_machine[3]==slot_machine[4]==slot_machine[5]:
+            if slot_machine[5][0]==slot_machine[6][0]==slot_machine[7][0]==slot_machine[8][0]:
                 #sets again the value of the input of win_1
-                again=win_1()
+                again=win()
             else:
                 again =lose()
 
         if rows == 2:
-            if slot_machine[3]==slot_machine[4]==slot_machine[5]:
+            if slot_machine[5][0]==slot_machine[6][0]==slot_machine[7][0]==slot_machine[8][0]==slot_machine[9][0]:
                 again=win_1()
-            elif slot_machine[0]==slot_machine[1]==slot_machine[2]:
+            elif slot_machine[0][0]==slot_machine[1][0]==slot_machine[2][0]==slot_machine[3][0]==slot_machine[4][0]:
                 again =win_1()
             else:
                again = lose()
  
         if rows == 3:
-            if slot_machine[3]==slot_machine[4]==slot_machine[5]:
-                again =win_1()
-            elif slot_machine[1]==slot_machine[2]==slot_machine[3]:
-                again =win_1()
-            elif slot_machine[6]==slot_machine[7]==slot_machine[8]:
-                again =win_1()
+            if slot_machine[5][0]==slot_machine[6][0]==slot_machine[7][0]==slot_machine[8][0]==slot_machine[9][0]:
+                again=win()
+            elif slot_machine[0][0]==slot_machine[1][0]==slot_machine[2][0]==slot_machine[3][0]==slot_machine[4][0]:
+                again =win()
+            elif slot_machine[10][0]==slot_machine[11][0]==slot_machine[12][0]==slot_machine[13][0]==slot_machine[14][0]:
+                again =win()
             else:
-                again =lose()
+                again = lose()
+
+        if rows == 4:
+            if slot_machine[5][0]==slot_machine[6][0]==slot_machine[7][0]==slot_machine[8][0]==slot_machine[9][0]:
+                again=win()
+            elif slot_machine[0][0]==slot_machine[1][0]==slot_machine[2][0]==slot_machine[3][0]==slot_machine[4][0]:
+                again =win()
+            elif slot_machine[10][0]==slot_machine[11][0]==slot_machine[12][0]==slot_machine[13][0]==slot_machine[14][0]:
+                again =win()
+            elif slot_machine[0][0]==slot_machine[4][0]==slot_machine[6][0]==slot_machine[8][0]==slot_machine[12][0]:
+                again=win()
+            else:
+                again = lose()
+
+        if rows == 5:
+            if slot_machine[5][0]==slot_machine[6][0]==slot_machine[7][0]==slot_machine[8][0]==slot_machine[9][0]:
+                again=win()
+            elif slot_machine[0][0]==slot_machine[1][0]==slot_machine[2][0]==slot_machine[3][0]==slot_machine[4][0]:
+                again =win()
+            elif slot_machine[10][0]==slot_machine[11][0]==slot_machine[12][0]==slot_machine[13][0]==slot_machine[14][0]:
+                again =win()
+            elif slot_machine[0][0]==slot_machine[4][0]==slot_machine[6][0]==slot_machine[8][0]==slot_machine[12][0]:
+                again=win()
+            elif slot_machine[2][0]==slot_machine[6][0]==slot_machine[8][0]==slot_machine[10][0]==slot_machine[14][0]:
+                 again=win()
+            else:
+                again = lose()
+
         
 #when while loop is broken thank user
 print("Thanks for playing, see you next time!")
