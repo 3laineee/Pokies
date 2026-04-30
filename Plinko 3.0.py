a24bab13e35950c16e1a65f2a1df4ea5fa0434bf
3laineee <chenel@wgc.school.nz>
2026-04-30 14:50:05 +1200

    Add files via upload
    made sure there no crashes and added user info

--- Plinko 3.0.py
+++ Plinko 3.0.py
@@ -17,6 +17,7 @@ loop = 0
 balance = 1000
 again = 'T'
 bet = 0
+lifetime_losses = 0
 
 
 def win():
@@ -37,14 +38,25 @@ def lose():
     print("your new balance is {}".format(balance))
     #sends the info 'outside' of the function
 
+
 def number_asker():
     """
     asks the user for values
     """
     print("Your current balance is ${}.".format(balance))
-    rows=int(input(("How much rows are you betting?(1-5):$ ")))
-    bet=int(input("How much money are you betting?: "))
-    return rows, bet
+    rows = 0
+    bet = 0
+
+    again = True
+    while again == True:
+        try:
+            rows=int(input(("How much rows are you betting?(1-5):$ ")))
+            bet=int(input("How much money are you betting?: "))
+            again == False
+            return rows, bet
+        except ValueError:
+            print("Please pick a number!")
+
 
 def value_checker(rows,bet):
     """
@@ -142,10 +154,25 @@ def win_checker(row):
         else:
             lose()
 
+def user_info():
+    """
+    asks user for info and returns
+    """
+    again = True
+    while again == True:
+        try:
+            name = input("What is your name?: ").upper()
+            location = input("Which region are you playing from?: ").upper()
+            again == False
+            return name, location
+        except ValueError:
+            print("Please use your real name and location.")
 
-        
+
 #main routine
 if __name__ == "__main__":
+    #asks for user info
+    name1, location1 = user_info()
     while again == 'T':    
         #shuffles the pokies
         shuffle()
@@ -159,8 +186,16 @@ if __name__ == "__main__":
         pokies()
         #checks if user won
         win_checker(row)
+
         #asks user to play again
-        again = input("Would you like to play again? T or F: ").upper()
+        repeat = True
+        while repeat == True: #keeps looping untill get T or F
+            again = input("Would you like to play again? T or F: ").upper()
+            if again != 'T' and again != 'F': 
+                print("Please choose another value!")
+            else:
+                repeat = False
+            
 
     print("Thank you for playing!") #thank user for playing
 
